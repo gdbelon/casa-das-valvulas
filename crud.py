7a144dbebefeb357a125d09b8bb49445d6c9cda2
gdbelon <guidiasbelon@gmail.com>
2026-07-02 11:54:32 -0300

    Mais bugs corrigidos

--- crud.py
+++ crud.py
@@ -1,21 +1,23 @@
 from database import get_connection
 from psycopg2.extras import RealDictCursor
 
-def cadastrar_produto(nome,qtd,marca):
+def cadastrar_produto(nome, qtd, marca):
     conn = get_connection()
     cur = conn.cursor(cursor_factory=RealDictCursor)
     try:
-        cur.execute('SELECT * FROM produtos WHERE nome_valvula = %s AND marca = %s',(nome,marca))
+        cur.execute('SELECT * FROM produtos WHERE nome_valvula = %s AND marca = %s', (nome, marca))
         row = cur.fetchone()
         if row is not None:
-            return{"mensagem" : "Produto já existe"}
-        cur.execute('INSERT INTO produtos (nome_valvula,qtd,marca) VALUES (%s,%s,%s)',(nome,qtd,marca))
+            return {"mensagem": "Produto já existe"}
+        cur.execute(
+            'INSERT INTO produtos (nome_valvula,qtd,marca) VALUES (%s,%s,%s) RETURNING *',(nome, qtd, marca)
+        )
         row_2 = cur.fetchone()
         conn.commit()
-        return{"mensagem" : row_2 }
+        return {"mensagem": row_2}
     except Exception as e:
         conn.rollback()
-        return{e}
+        return {"erro": str(e)}
     finally:
         cur.close()
         conn.close()
@@ -26,10 +28,10 @@ def listar_valvulas():
     try:
         cur.execute('SELECT * FROM produtos')
         rows = cur.fetchall()
-        return{"mensagem" : rows}
+        return {"mensagem": rows}
     except Exception as e:
         conn.rollback()
-        return{"erro": e}
+        return {"erro": str(e)}
     finally:
         cur.close()
         conn.close()
@@ -41,46 +43,51 @@ def listar_uma_valvula(nome):
         cur.execute("SELECT * FROM produtos WHERE nome_valvula ~* %s", (f"^{nome}",))
         row = cur.fetchone()
         if row is None:
-            return{"mensagem" : "Nenhum Produto foi encontrado"}
-        return{"mensagem" : row}
+            return {"mensagem": "Nenhum Produto foi encontrado"}
+        return {"mensagem": row}
     except Exception as e:
         conn.rollback()
-        return{"erro" : str(e)}
+        return {"erro": str(e)}
     finally:
         cur.close()
         conn.close()
 
-def atualizar_nome_valvula(id,nome):
+
+def atualizar_nome_valvula(id, nome):
     conn = get_connection()
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
     try:
-        cur.execute('SELECT * FROM produtos WHERE nome_valvula = %s AND id = %s',(nome,id))
+        cur.execute('SELECT * FROM produtos WHERE nome_valvula = %s AND id = %s', (nome, id))
         row = cur.fetchone()
-        if row is True:
-            return{"mensagem" : "Já existe um produto com esse nome"}
-        cur.execute('UPDATE produtos SET nome_valvula = %s WHERE id = %s',(nome,id))
+        if row is not None:
+            return {"mensagem": "Já existe um produto com esse nome"}
+        cur.execute(
+            'UPDATE produtos SET nome_valvula = %s WHERE id = %s RETURNING *',(nome, id)
+        )
         row_2 = cur.fetchone()
         conn.commit()
-        return{"mensagem" : row_2}
+        return {"mensagem": row_2}
     except Exception as e:
         conn.rollback()
-        return{"erro" : str(e)}
+        return {"erro": str(e)}
     finally:
         cur.close()
         conn.close()
 
-def atualizar_qtd(id,qtd):
+
+def atualizar_qtd(id, qtd):
     conn = get_connection()
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
     try:
-        cur.execute('UPDATE produtos SET qtd = %s WHERE id = %s',(qtd,id))
+        cur.execute(
+            'UPDATE produtos SET qtd = %s WHERE id = %s RETURNING *',(qtd, id)
+            )
         row = cur.fetchone()
         conn.commit()
-        return{"mensagem" : row}
+        return {"mensagem": row}
     except Exception as e:
         conn.rollback()
-        return{"erro" : str(e)}
+        return {"erro": str(e)}
     finally:
         cur.close()
         conn.close()
-        
